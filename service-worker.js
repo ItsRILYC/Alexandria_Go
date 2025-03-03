9081ce5a26f3913048dae4389e2b4605eecc86bf
ItsRILYC <112127040+ItsRILYC@users.noreply.github.com>
2025-03-03 23:37:30 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'alexandria-go-cache-v5';
+const urlsToCache = [
+  'index.html',
+  'styles.css',
+  'app.js',
+  'manifest.json',
+  'favicon.png',
+  'icons/training.svg',
+  'icons/team.svg',
+  'icons/seasons.svg',
+  'icons/edit.svg',
+  'icons/delete.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
